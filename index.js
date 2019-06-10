92b49988bfc135f7dacdc90f8e3cd457485dc69c
Ibai Seco <ibaiseco@Ibais-MacBook-Pro.local>
2019-06-10 10:45:18 -0400

    Implemented delete feature, provided some visual improvements, and implemented a confirmation feature

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 
 const repo = require('./lib/repo');
 const github = require('./lib/github');
-const { envRepo, envOrg, envToken } = require('./lib/config');
+const chalk = require('chalk');
+const { envRepo, envOrg, envToken, envBranch } = require('./lib/config');
 
 const repos = envRepo;
 const org = envOrg;
 const token = envToken;
+const branch = envBranch;
 
-console.log('Welcome to the git tagger!');
+console.log(chalk.bold('Welcome to Your Release Organizer!'));
 
 const run = async () => {
-    const versionTag = await github.getQuestions();
-    const post = await repos.map(x => repo.getRepos(x, versionTag.tagVersion, versionTag.releaseName, versionTag.description, org, token));
-    console.log('Complete.')
+    const funcOption = await github.getOption();
+    if (funcOption.function == 'New Release') {
+        const versionTag = await github.getQuestions();
+        const releaseConfirm = await github.getConfirmation();
+        if (releaseConfirm.confirm == 'Yes') {
+            const post = await repos.map(x => repo.getRepos(x, versionTag.tagVersion, versionTag.releaseName, versionTag.description, org, branch, token));
+            console.log(chalk.green('Release complete.'));
+        }
+        else {
+            console.log(chalk.red('Release cancelled!'));
+        }
+    } else {
+        const getRepos = await repo.forSelectRepo(org, token);
+        const reposOptions = await github.getRepositories(getRepos);
+        select = reposOptions.repos;
+        tagChoices = await repo.getReleases(select, org, token);
+        const tag = await github.getTags(tagChoices);
+        ///
+        const releaseConfirm = await github.getConfirmation();
+        if (releaseConfirm.confirm == 'Yes') {
+            const releases = await repo.getReleases(select, org, token);
+            const removeTag = await repo.deleteRelease(tag.releases, select, org, token);
+            console.log(chalk.green('Release deleted.'));
+        }
+        else {
+            console.log(chalk.red('Delete cancelled!'));
+        }
+    }
 }
 run();
 
